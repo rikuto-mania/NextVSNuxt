f41e3449d0a8f73f8327d0a571f4250a0bbe81d9
rikuto-mania <rikutoland55@gmail.com>
2026-04-24 20:08:50 +0900

    [add]メイン画面作成

--- nuxt/app/app.vue
+++ nuxt/app/app.vue
@@ -7,8 +7,28 @@ import Header from './components/Header.vue';
 <template>
   <div class="min-h-screen">
     <Header />
-    
+      <div class="h-120 bg-linear-to-t from-[#FF6A33] to-[#FFD900] ">
 
+      </div>
+
+      <section class=" max-w-5xl mx-auto py-16">
+        <div class="pb-8">
+          <p class="text-3xl">新しい商品</p>
+        </div>
+
+        <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 pb-10">
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+          <ProductCard title="SMCS" :price=2000 :reviews=2 />
+        </div>
+
+        <div class="flex justify-center"> 
+          <NuxtLink to="/products" class="text-white text-center bg-[#FF6A33] px-8 py-2.5">もっと見る</NuxtLink>
+        </div>
+      </section>
     <Footer />
   </div>
 </template>
--- nuxt/app/components/Header.vue
+++ nuxt/app/components/Header.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
     import LoginDialog from './Dialog/LoginDialog.vue';
     import {ref} from 'vue';
+import SearchBar from './SearchBar.vue';
 
     const isOpenDialog = ref(false)
 
@@ -10,9 +11,11 @@
 </script>
 
 <template>
-    <header class=" border-b border-gray-400 flex items-center justify-between px-3 py-2.5">
-        <p class="text-[#FF6A33] text-4xl font-bold">Rikushop</p>
-
+    <header class=" border-b border-gray-400 flex items-center justify-between px-6 py-5">
+        <div class="flex gap-4">
+            <p class="text-[#FF6A33] text-4xl font-bold">Rikushop</p>
+            <SearchBar />
+        </div>
         <div class="flex items-center gap-2">
             <p class="text-lg" @click="toggle">ログイン</p>
             <LoginDialog v-if="isOpenDialog" @close="isOpenDialog = false"/>
