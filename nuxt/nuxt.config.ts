b4f419242460c2d007c8205188dda7239bf7cdf5
rikuto-mania <rikutoland55@gmail.com>
2026-04-24 23:52:54 +0900

    [fix]head設定

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -2,6 +2,18 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app:{
+    head:{
+      title:"Rikushop",
+      meta:[
+        {name:"description", content:"rikuto shop"},
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      htmlAttrs:{
+        lang:"ja"
+      }
+    }
+  },
   css:['~/assets/css/main.css'],
   modules: [
     '@nuxt/icon'
