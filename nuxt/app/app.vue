db1da02693da38cf4d573573ebb1b6c6700e42bc
rikuto-mania <rikutoland55@gmail.com>
2026-04-29 23:28:56 +0900

    [fix]再コミット

---
+++ nuxt/app/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    <div class="flex-1">
+       <NuxtPage />
+    </div>
+    <Footer />
+  </div>
+</template>
---
+++ nuxt/app/components/submitButton.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+     interface butonProps{
+        value:string,
+    }
+
+    const props = defineProps<butonProps>();
+</script>
+
+<template>
+    <button type="submit" class="bg-[#FF6A33] text-white border py-2.5 w-full rounded-full">{{value}}</button>
+</template>
