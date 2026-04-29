2471266019c16028fd4ba0dd63b64b9778fcccf5
rikuto-mania <rikutoland55@gmail.com>
2026-04-29 23:28:09 +0900

    [fix].gitignoreを適用し、不要なファイルを追跡対象から外す

--- nuxt/app/components/SearchBar.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="w-90 relative">
-        <input type="text" class="pl-3 pr-12 py-2.5 w-[360px] bg-[#F2F1F1]" placeholder="商品を検索する"> 
-        <Icon name="material-symbols:search-rounded" style="color: black" size="2em" class="absolute right-3 top-1/2 -translate-y-1/2"/>
-    </div>
-</template>
