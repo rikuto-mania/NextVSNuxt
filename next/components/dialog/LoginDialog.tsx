6ebbfb153337e72fea17f1a7157bedced86e0edd
rikuto-mania <rikutoland55@gmail.com>
2026-05-14 12:05:57 +0900

    [fix]dialogの背景色をクリックできないバグの修正

--- next/components/dialog/LoginDialog.tsx
+++ next/components/dialog/LoginDialog.tsx
@@ -12,25 +12,24 @@ export const LoginDialog = () =>{
     }
 
     return(
-        <div className="flex justify-center p-4 z-20" onClick={(e) => e.stopPropagation()}>
-            <div className="px-8 py-9 w-131  md:rounded-l-2xl bg-white z-10">
-                <p className="font-bold text-2xl pb-12">{isLoginMode ? "ログイン" : "新規登録"}</p>
-                <TextInput label="メールアドレス" type="email" name="mail" id="mail" placeholder="exsample@rikushop.com"/>
-                <TextInput label="パスワード" type="password" name="password" id="password" />
-
-                <div v-if="!isLoginMode">
-                    <TextInput label="パスワード(確認)" type="password" name="passwordConfilm" id="passwordConfilm" />
+        <div className="flex justify-center p-4 z-20">
+            <div className="flex" onClick={(e) => e.stopPropagation()}>
+                <div className="px-8 py-9 w-131  md:rounded-l-2xl bg-white z-10">
+                    <p className="font-bold text-2xl pb-12">{isLoginMode ? "ログイン" : "新規登録"}</p>
+                    <TextInput label="メールアドレス" type="email" name="mail" id="mail" placeholder="exsample@rikushop.com"/>
+                    <TextInput label="パスワード" type="password" name="password" id="password" />
+                    {!isLoginMode && <TextInput label="パスワード(確認)" type="password" name="passwordConfilm" id="passwordConfilm" />}
+                    <div className="md:px-18.5">
+                        <SubmitButton value= {isLoginMode ? 'ログイン' : 'アカウントを作成'}/>
+                    </div>
+                    <p className="text-center  pt-4">{isLoginMode ? "アカウントはありませんか？" : "アカウントはありますか？"}
+                        <span className="text-[#FF6A33] rounded-4xl" onClick={() => toggleMenu()}>
+                            {isLoginMode ? "新規登録" : "ログイン"}
+                        </span>
+                    </p>
                 </div>
-                <div className="md:px-18.5">
-                    <SubmitButton value= {isLoginMode ? 'ログイン' : 'アカウントを作成'}/>
-                </div>
-                <p className="text-center  pt-4">{isLoginMode ? "アカウントはありませんか？" : "アカウントはありますか？"}
-                    <span className="text-[#FF6A33] rounded-4xl" onClick={() => toggleMenu()}>
-                        {isLoginMode ? "新規登録" : "ログイン"}
-                    </span>
-                </p>
+                <div className="bg-[#FF6A33] w-79 h-auto rounded-r-2xl hidden md:block"></div>
             </div>
-            <div className="bg-[#FF6A33] w-79 h-auto rounded-r-2xl hidden md:block"></div>
         </div>
     );
 }
