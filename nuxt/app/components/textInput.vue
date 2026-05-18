6ab1a6ae2a4f96f54be616a093a61b2f1eb5d87e
rikuto-mania <rikutoland55@gmail.com>
2026-05-18 12:31:39 +0900

    [update]ログイン処理の結合完成

--- nuxt/app/components/textInput.vue
+++ nuxt/app/components/textInput.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
     interface formProps{
+        modelValue: string;
         label:string,
         type: "text" | "email" | "password",
         name:string,
@@ -9,18 +10,28 @@
     }
 
     const props = defineProps<formProps>()
+    
+    //emit 子コンポーネントから親コンポーネントへデータを渡したり、イベントを通知したりするための機能
+    //親コンポーネントへ「modelValue」イベントを送信
+    const emit = defineEmits<{
+        (e:"update:modelValue",value:string) : void
+    }>()
 </script>
 
 <template>
     <div class="flex flex-col pb-6">
         <label :for="id" class="pb-3">{{label}}</label>
         <input 
+            @input="emit('update:modelValue',($event.target as HTMLInputElement).value)"
             :type="type"
             :name="name"
             :id="id"
             :placeholder="placeholder" 
-            :value="value"
+            :value="modelValue",
             class="px-3 py-2.5 rounded-lg border-2 border-[#BBB7B7] w-full"
         >
     </div>
 </template>
+
+//MEMO
+//emit子コンポーネントから親コンポーネントへデータを渡したり、イベントを通知したりするための機能
