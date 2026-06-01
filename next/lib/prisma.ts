cd30db0129e00a43ed175114270a768dd531517b
rikuto-mania <rikutoland55@gmail.com>
2026-06-01 21:55:48 +0900

    [update]nuxtで作ったテーブルを再利用するためスキーマを編集

--- next/lib/prisma.ts
+++ next/lib/prisma.ts
@@ -1,21 +1,21 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 //Mysqlコネクションアダプター
 const adapter = new PrismaMariaDb({
     user:process.env.DB_USER,
-    host:process.env.DB_HOST,
+    port:Number(process.env.DB_PORT),
     database:process.env.DB_NAME,
     password:process.env.DB_PASSWORD,
     allowPublicKeyRetrieval: true,
-    connectionLimit:5
+    connectionLimit:20
 })
 
 //Nodejsのグローバルスコープにprismaを保存するための準備
 export const globalForPrisma = globalThis as unknown as{ prisma: PrismaClient | undefined; }
 
 //prismaインスタンスの作成または再利用
-export const prisma = adapter ?? new PrismaClient(adapter);
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({adapter});
 
 //本番環境でなけれなばPrismaClientをglobalThisに保存
 if(process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
