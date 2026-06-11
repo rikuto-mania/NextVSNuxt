ab12408de96ecf2da758d6b584fb4d35c284d117
rikuto-mania <rikutoland55@gmail.com>
2026-06-12 01:50:33 +0900

    [fix]商品apiに平均評価を追加

--- next/app/api/product/[id]/route.ts
+++ next/app/api/product/[id]/route.ts
@@ -10,7 +10,7 @@ export async function GET(req:NextRequest,context:{params: Promise<{id:string}>}
 
     if (!productid) return NextResponse.json({status:"error",code:404,message:"商品がみつかりませんでした"},{status:404});
     try{
-        const item =await prisma.product.findUnique({
+        const products =await prisma.product.findUnique({
             where: {id:productid},
             include:{
                 _count:{
@@ -18,9 +18,22 @@ export async function GET(req:NextRequest,context:{params: Promise<{id:string}>}
                 },
                 Image:true,
             }
-        })
+        });
 
-        return NextResponse.json({status:"success",code:200,data:item},{status:200});
+        //平均評価取得
+        const reviewAvg = await prisma.review.groupBy({
+            by:["productId"],
+            _avg:{
+                level:true
+            },
+         });
+         
+        const rating = reviewAvg.find( r => r.productId === productid);
+         
+        //商品データ、平均評価を含めたオブジェクト作成
+        const result = {...products,avgLevel:rating?._avg.level}
+    
+        return NextResponse.json({status:"success",code:200,data:result},{status:200})
     }catch(error){
         return NextResponse.json({status:"error",code:500,message:"サーバーエラーが発生しました"},{status:500});
     }
--- next/app/api/product/route.ts
+++ next/app/api/product/route.ts
@@ -7,7 +7,7 @@ export async function GET(req:NextRequest) {
     const searchWord = req.nextUrl.searchParams.get("searchWord");
     const where = searchWord ? {name:{contains:searchWord}} : {};
     try{
-         const item = await prisma.product.findMany({
+         const products = await prisma.product.findMany({
             where:where,
             orderBy:{
                 created_at:"desc", 
@@ -16,12 +16,29 @@ export async function GET(req:NextRequest) {
                 _count:{
                     select:{Review:true},
                 },
-                Image:true,
+                Image:true
             }
          });
 
+         //平均評価取得
+         const reviewAvg = await prisma.review.groupBy({
+            by:["productId"],
+            _avg:{
+                level:true
+            },
+         })
+         console.log(reviewAvg);
+        
+         //商品データ、平均評価を含めたオブジェクト作成
+         const result = products.map(product =>{
+            const rating = reviewAvg.find(
+                r => r.productId === product.id
+            );
 
-        return NextResponse.json({status:"success",code:200,data:item},{status:200})
+            return  {...product,avgLevel:rating?._avg.level};
+         })
+
+        return NextResponse.json({status:"success",code:200,data:result},{status:200})
     }catch(error){
         return NextResponse.json({status:"error",code:500,message:"サーバーエラーが発生しました",error: error instanceof Error ? error.message : String(error)},{status:500})
     }
