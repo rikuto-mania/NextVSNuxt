1fd35e76fe5370f5b09200fb6184ef6e8ed60a6e
rikuto-mania <rikutoland55@gmail.com>
2026-06-15 23:47:04 +0900

    [update]ログインフォーム結合

--- next/components/textInput.tsx
+++ next/components/textInput.tsx
@@ -1,16 +1,23 @@
+import React from "react";
+
  interface formProps{
         label:string,
-        type: "text" | "email" | "password",
-        name:string,
-        id:string,
-        placeholder?:string,
-        value?: string, 
+        type: "text" | "email" | "password";
+        name:string;
+        id:string;
+        placeholder?:string;
+        error?:string;
+        value?: string;
+        onChange?:(e:React.ChangeEvent<HTMLInputElement>) => void;
     }
 
-export const TextInput = ({label,type,name,id,placeholder,value}:formProps) =>{
+export const TextInput = ({label,type,name,id,placeholder,error,value,onChange}:formProps) =>{
     return (
         <div className="flex flex-col pb-6">
-            <label htmlFor={id} className="pb-3">{label}</label>
+            <div className="flex gap-6">
+                <label htmlFor={id} className="pb-3">{label}</label>
+               {error &&  <p className="text-red-400">{error}</p>}
+            </div>
             <input 
                 type={type}
                 name={name}
@@ -18,6 +25,7 @@ export const TextInput = ({label,type,name,id,placeholder,value}:formProps) =>{
                 placeholder={placeholder} 
                 value={value}
                 className="px-3 py-2.5 rounded-lg border-2 border-[#BBB7B7] w-full"
+                onChange={onChange}
              />
         </div>
     );
