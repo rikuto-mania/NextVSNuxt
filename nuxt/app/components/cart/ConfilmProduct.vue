724e75fad469aec506091de4b45567c44d378f04
rikuto-mania <rikutoland55@gmail.com>
2026-06-25 23:55:25 +0900

    [add]購入確認画面追加

---
+++ nuxt/app/components/cart/ConfilmProduct.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+    interface productCardProps{
+        image?:string;
+        title:string;
+        price:number;
+        reviews:number;
+    }
+    
+    const props = defineProps<productCardProps>();
+
+    const imagepath = "/products/" + props.image;
+</script>
+
+<template>
+     <div class="flex justify-between items-center gap-6 w-full h-30">
+       <div class="flex items-center">
+            <div class="flex justify-center items-center bg-[#F2F1F1] w-30 h-30 rounded-2xl">
+                <NuxtImg :src="imagepath" width="100" height="auto"/>
+            </div>
+            <div class="pl-3 md:pl-6 md:text-2xl">
+                <p class="font-medium pb-3">{{title}}</p>
+                <p class="font-bold">¥<span class="pl-1.5 text-[#FF6A33]">{{price}}</span></p>
+            </div>
+       </div>
+        <p>数量：<span>11</span></p>
+    </div>
+</template>
---
+++ nuxt/app/pages/cart/confilm/index.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+import ConfilmProduct from '~/components/cart/ConfilmProduct.vue';
+
+    const dummyResponse = [
+        {
+            title:"macbookair 2020 intel silver",
+            price:200000,
+            review:4.5
+        },
+        {
+            title:"ddd",
+            price:20,
+            review:4
+        }
+    ]
+</script>
+
+<template>
+    <section class="max-w-5xl mx-auto px-4 py-10">
+         <div class="pb-8 flex gap-2">
+            <div class="bg-[#FF6A33] w-1 h-auto"></div>
+            <h2 class="text-3xl">購入確認</h2>
+        </div>
+
+        <div class="flex gap-28.5 flex-col lg:flex-row-reverse justify-between">
+            <div>
+                <p>合計金額</p>
+                <p class="font-bold text-3xl pb-9">¥<span class="pl-1.5 text-[#FF6A33]">20000</span></p>
+                 <button class="w-full lg:w-3xs py-2.5 text-white bg-[#FF6A33]">購入する</button>
+            </div>
+            <div class="flex flex-col xl:w-170">
+                <div  v-for="(item,index) in dummyResponse" :key="index" class="flex flex-col">
+                    <ConfilmProduct :title="item.title" :price="item.price" :reviews="item.review" />
+                    <hr v-if="index !== dummyResponse.length -1" class="border border-[#BBB7B7] my-6"> 
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
