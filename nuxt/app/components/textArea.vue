17dd1d2b4ae2bf5dfef50c4ce8c5985174f26a38
rikuto-mania <rikutoland55@gmail.com>
2026-07-13 16:28:45 +0900

    [add]レビュー投稿画面追加

---
+++ nuxt/app/components/textArea.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+    interface formProps{
+        label:string,
+        name:string,
+        id:string,
+        placeholder?:string,
+        value?: string, 
+    }
+
+    const props = defineProps<formProps>()
+    
+    //emit 子コンポーネントから親コンポーネントへデータを渡したり、イベントを通知したりするための機能
+    //親コンポーネントへ「modelValue」イベントを送信
+    const emit = defineEmits<{
+        (e:"update:modelValue",value:string) : void
+    }>()
+</script>
+
+<template>
+    <div class="flex flex-col pb-6">
+        <label :for="id" class="pb-3">{{label}}</label>
+        <textarea 
+            @input="emit('update:modelValue',($event.target as HTMLInputElement).value)"
+            :name="name"
+            :id="id"
+            :placeholder="placeholder" 
+            class="px-3 py-2.5 rounded-lg border-2 border-[#BBB7B7] w-full"
+         />
+    </div>
+</template>
+
+//MEMO
+//emit子コンポーネントから親コンポーネントへデータを渡したり、イベントを通知したりするための機能
---
+++ nuxt/app/pages/products/[id]/review.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+    import textArea from '~/components/textArea.vue';
+    import submitButton from '~/components/submitButton.vue';
+
+    //リクエストデータ
+    const level = ref(5);
+    const description = ref("");
+
+    //商品ID取得
+    const route = useRoute();
+    const productId = Number(route.params.id);
+</script>
+
+<template>
+    <section class="px-4 py-10 max-w-4xl mx-auto">
+        <div  class="pb-8 flex gap-2">
+            <div class="bg-[#FF6A33] w-1 h-auto"></div>
+            <p class="text-3xl">レビューを投稿</p>   
+        </div>
+        <div>
+            <p>評価</p>
+            <div class="flex flex-row">
+                <Icon @click="level = n" v-for="n in 5" :name="n <= level? 'material-symbols:star-rounded' :'material-symbols:star-outline-rounded'" style="color: gold" size="48px" class=""/>    
+            </div>
+        </div>
+        <textArea v-model="description" label="レビュー" id="review" name="review"/>
+        <submitButton value="投稿する" :url="`/review/${productId}`" method="POST" :body="{level,description}" :redirectUrl="`/products/${productId}`" />
+    </section>
+</template>
+
+
