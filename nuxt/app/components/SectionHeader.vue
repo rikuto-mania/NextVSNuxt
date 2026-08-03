6bd5c2f6e8dda2d410b96a7e38a3b558cc0aadf0
rikuto-mania <rikutoland55@gmail.com>
2026-08-03 12:19:46 +0900

    [fix]nuxtjsセクションタイトルのクラス修正

--- nuxt/app/components/SectionHeader.vue
+++ nuxt/app/components/SectionHeader.vue
@@ -9,7 +9,7 @@
 
 <template>
     <div class="pb-8 flex gap-2">
-        <div className="bg-[#FF6A33] w-1 h-auto"></div>
+        <div class="bg-[#FF6A33] w-1 h-auto"></div>
         <h2 class="text-3xl">{{props.title}}</h2>
     </div>
 </template>
